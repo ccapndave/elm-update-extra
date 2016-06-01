e7cb3065d1ee98d1dc72c9e6001d54d02e3cb949
Nick ten Veen <nveen0930@gmail.com>
2016-06-01 17:02:30 +0200

    More features added to enable pipelining of messages in updates
      * Added `andThen`
      * Added `filter`
      * Added `batch`
      * Added `addCmd`

--- src/Update/Extra.elm
+++ src/Update/Extra.elm
@@ -1,8 +1,18 @@
-module Update.Extra exposing (..)
+module Update.Extra exposing
+  ( pipeUpdate
+  , andThen
+  , filter
+  , addCmd
+  , batch
+  )
 
 {-| Convenience functions for working with effects in Elm
 
 @docs pipeUpdate
+@docs andThen
+@docs filter
+@docs addCmd
+@docs batch
 -}
 
 {-| A function allowing you to compose calls to update.  Most useful when used
@@ -25,3 +35,87 @@ pipeUpdate (model, cmd) f =
     (model', cmd') = f model
   in
   (model', Cmd.batch [ cmd, cmd' ])
+
+{-| Allows update call composition. Identical to [`pipeUpdate`](#pipeUpdate),
+but with reversed arguments to be able to use it with the pipeline operator (`|>`)
+
+For example:
+```elm
+update msg model =
+  model ! []
+    |> andThen (update SomeMessage)
+    |> andThen (update SomeOtherMessage)
+    ...
+```
+-}
+andThen : (model -> (model, Cmd msg)) -> (model, Cmd msg) -> (model, Cmd msg)
+andThen update (model, cmd) =
+  let
+    (model', cmd') = update model
+  in
+    (model', Cmd.batch [cmd, cmd'])
+
+{-| Allows you to conditionally trigger updates based on a predicate. Can be
+used with the pipeline operator.
+
+For example:
+```elm
+update msg model =
+  case msg of
+    SomeMessage i ->
+      model ! []
+        |> filter (i > 10)
+            (    andThen (update BiggerThanTen)
+              >> andThen (update AnotherMessage)
+              >> andThen (update EvenMoreMessages)
+            )
+        |> andThen (update AlwaysTriggeredAfterPredicate)
+```
+
+If you want to the pipeline operator in the nested pipeline, consider a lambda:
+```elm
+...
+|> filter (i > 10)
+  ( \state -> state
+      |> andThen (update BiggerThanTen)
+      |> andThen (update AnotherMessage)
+      |> andThen (update EvenMoreMessages)
+  )
+|> andThen (update AlwaysTriggeredAfterPredicate)
+```
+-}
+filter : Bool -> ((model, Cmd msg) -> (model, Cmd msg)) -> ((model, Cmd msg) -> (model, Cmd msg))
+filter pred f =
+  if pred then
+    f
+  else
+    identity
+
+{-| allows you to attach a Cmd to an update pipeline.
+
+```elm
+update msg model = model ! []
+  |> andThen (update AMessage)
+  |> addCmd doSomethingWithASideEffect
+```
+-}
+addCmd : Cmd msg -> (model, Cmd msg) -> (model, Cmd msg)
+addCmd cmd' (model, cmd) = (model, Cmd.batch [cmd, cmd'])
+
+{-| allows you to attach multiple messages to an update at once.
+
+```elm
+update msg model = model ! []
+  |> batch update
+    [ AMessage
+    , AnotherMessage
+    , AThirdMessage
+    ]
+```
+-}
+batch : (msg -> model -> (model, Cmd msg)) -> List msg -> (model, Cmd msg) -> (model, Cmd msg)
+batch update msgs init =
+  let
+    foldUpdate = andThen << update
+  in
+    List.foldl foldUpdate init msgs
